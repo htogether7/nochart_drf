b5ba9bc9a49bfc4232bf1ac23b6279fb970d8e1c
htogether7 <htogether7@gmail.com>
2023-02-10 01:18:20 +0900

    DRF 세팅

--- nochart/settings.py
+++ nochart/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mystock'
+    'mystock',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
